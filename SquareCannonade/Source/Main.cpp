b4c8b0b6220d28c38e76aecae11f5a2c73d864d3
aaronistheman <aarons.7007@gmail.com>
2016-10-02 16:06:19 -0700

    Added exception handler to Main

--- SquareCannonade/Source/Main.cpp
+++ SquareCannonade/Source/Main.cpp
@@ -1,52 +1,22 @@
+/**
+ * Based on version written by: Artur Moreira, Henrik Vogelius Hansson, and
+ *   Jan Haller
+ */
+
 #include <Application.hpp>
-#include <Game/Test.hpp>
-#include <Game/Test2.hpp>
+
+#include <stdexcept>
+#include <iostream>
 
 int main()
 {
-  Application app;
-  app.run();
-  /*
-  sf::RenderWindow window(sf::VideoMode(1200, 600), "Square Cannonade");
-
-  TextureHolder textures;
-  textures.load(Textures::Test, "Media/junk.png");
-
-  FontHolder fonts;
-  fonts.load(Fonts::Main, "Media/Sansation.ttf");
-
-  State* state = new TitleState(window, fonts, textures);
-  */
-  /*
-  Controller controller;
-  controller.handleRealTimeInput(titleState);
-  controller.handleEvent(titleState);
-  */
-  /*
-  while (window.isOpen())
+  try
   {
-    sf::Event event;
-    while (window.pollEvent(event))
-    {
-      bool keepState = state->handleEvent(event);
-      if (!keepState)
-      {
-        // Delete the TitleState,
-        // then point the current state at a new GameState
-        delete state;
-        state = new GameState(window, textures);
-      }
-      else
-        state->handleRealTimeInput();
-
-      if (event.type == sf::Event::Closed)
-        window.close();
-    }
-
-    window.clear();
-    state->draw();
-    window.display();
+    Application app;
+    app.run();
   }
-  */
-  return 0;
+  catch (std::exception& e)
+  {
+    std::cerr << "\nEXCEPTION: " << e.what() << std::endl;
+  }
 }
