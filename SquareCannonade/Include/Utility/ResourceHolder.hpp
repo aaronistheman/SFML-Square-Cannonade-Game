bc30c913dcadaa48da30dbc4838f123158594b44
aaronistheman <aarons.7007@gmail.com>
2016-10-01 23:15:13 -0700

    Imported ResourceHolder class from external (cited) source

---
+++ SquareCannonade/Include/Utility/ResourceHolder.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+/*
+ * Authors: Artur Moreira, Henrik Vogelius Hansson, and
+ *   Jan Haller
+ */
+
+#include <map>
+#include <string>
+#include <memory>
+#include <stdexcept>
+#include <cassert>
+
+
+template <typename Resource, typename Identifier>
+class ResourceHolder
+{
+	public:
+		void						load(Identifier id, const std::string& filename);
+
+		template <typename Parameter>
+		void						load(Identifier id, const std::string& filename, const Parameter& secondParam);
+
+		Resource&					get(Identifier id);
+		const Resource&				get(Identifier id) const;
+
+
+	private:
+		void						insertResource(Identifier id, std::unique_ptr<Resource> resource);
+
+
+	private:
+		std::map<Identifier, std::unique_ptr<Resource>>	mResourceMap;
+};
+
+#include "ResourceHolder.inl"
