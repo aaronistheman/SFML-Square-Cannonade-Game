3c94a05f804e6c7705725ee59b68c5d4454cfd96
aaronistheman <aarons.7007@gmail.com>
2016-10-09 22:25:56 -0700

    Test is able to call World::createGrid()
    I had to edit the Visual Studio test project's source filter (that is,
    which files are in it).

--- SquareCannonadeTest/Source/TestWorldGridCreation.cpp
+++ SquareCannonadeTest/Source/TestWorldGridCreation.cpp
@@ -5,12 +5,19 @@
 
 #include <vector>
 
-TEST_CASE("Created correct number of tiles") {
+TEST_CASE("Created correct number of tiles #1") {
 
   std::vector<Tile::Ptr> tileGrid;
+  sf::IntRect rect(0, 0, 1200, 600);
+  int tileLength = 30;
+
+  World::createGrid(tileGrid, rect, tileLength);
+
   REQUIRE(tileGrid.size() == 2);
 }
 
+/*
 TEST_CASE("Correct positions of tiles") {
 
 }
+*/
