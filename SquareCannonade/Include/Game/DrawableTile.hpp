48c13936194316604e2f02c9448999aeef89472d
aaronistheman <aarons.7007@gmail.com>
2016-10-09 18:49:20 -0700

    Grid tile can be drawn

--- SquareCannonade/Include/Game/DrawableTile.hpp
+++ SquareCannonade/Include/Game/DrawableTile.hpp
@@ -3,12 +3,14 @@
 #include <Game/Tile.hpp>
 
 #include <SFML/Graphics/Drawable.hpp>
+#include <SFML/Graphics/RectangleShape.hpp>
 
 #include <memory>
 
 /**
  * This class is strictly for debugging purposes
- * (so I can see where a certain tile is).
+ * (so I can see where a certain tile is) and is
+ * thus a little wasteful memory-wise.
  */
 class DrawableTile : public Tile, public sf::Drawable
 {
@@ -21,5 +23,5 @@ public:
   virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
 
 private:
-
+  sf::RectangleShape mWhatToDraw;
 };
--- SquareCannonade/Source/DrawableTile.cpp
+++ SquareCannonade/Source/DrawableTile.cpp
@@ -1,14 +1,17 @@
 #include <Game/DrawableTile.hpp>
+// #include <Utility/Utility.hpp>
 
-#include <iostream>
+#include <SFML/Graphics/RenderTarget.hpp>
 
 DrawableTile::DrawableTile(int left, int top, int width, int height)
   : Tile(left, top, width, height)
+  , mWhatToDraw(sf::Vector2f(width, height))
 {
-
+  // centerOrigin(mWhatToDraw);
+  mWhatToDraw.setPosition(left, top);
 }
 
 void DrawableTile::draw(sf::RenderTarget& target, sf::RenderStates states) const
 {
-  std::cout << "DrawableTile::draw()\n";
+  target.draw(mWhatToDraw);
 }
