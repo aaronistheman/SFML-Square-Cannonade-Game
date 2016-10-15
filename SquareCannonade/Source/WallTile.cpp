b7977f78c216e230f43cade7822e151538e88294
aaronistheman <aarons.7007@gmail.com>
2016-10-15 10:20:30 -0700

    Got tests to compile again

--- SquareCannonade/Source/WallTile.cpp
+++ SquareCannonade/Source/WallTile.cpp
@@ -1,7 +1,6 @@
 #include <Tile/WallTile.hpp>
 
 #include <SFML/Graphics/RenderTarget.hpp>
-#include <SFML/Graphics/Transformable.hpp>
 
 WallTile::WallTile(int left, int top, int width, int height)
   : Tile(left, top, width, height, TileType::Wall, INT_MAX)
