c00174ed4dd2edd23a0d87b223b875c315a822cd
aaronistheman <aarons.7007@gmail.com>
2016-10-20 22:31:44 -0700

    Changed for loop to foreach loop in PathfindingGraph::createVertices()

--- SquareCannonade/Source/PathfindingGraph.cpp
+++ SquareCannonade/Source/PathfindingGraph.cpp
@@ -19,17 +19,14 @@ int PathfindingGraph::getNumEdges() const
 
 void PathfindingGraph::createVertices(const std::vector<Tile::Ptr> &tileGrid)
 {
-  // Can't seem to use foreach loop here
-  for (auto itr = tileGrid.begin(); itr != tileGrid.end(); ++itr)
+  for (auto const& tile : tileGrid)
   {
-    Tile* tile = itr->get();
-
     // Only bother creating a vertex if that vertex can be used by
     // a traveling entity
     if (tile->isTraversable())
     {
       PGVertex *vertex = new PGVertex;
-      vertex->tile = itr->get();
+      vertex->tile = tile.get();
       mVertices.push_back(PGVertex::Ptr(vertex));
     }
   }
