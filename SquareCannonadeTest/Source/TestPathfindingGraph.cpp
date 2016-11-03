1675242dd137edf6d5d9c36995ec1e5ffa55023f
aaronistheman <aarons.7007@gmail.com>
2016-11-02 21:49:52 -0700

    Finished tests for setting both start and end vertices

--- SquareCannonadeTest/Source/TestPathfindingGraph.cpp
+++ SquareCannonadeTest/Source/TestPathfindingGraph.cpp
@@ -295,6 +295,9 @@ std::unique_ptr<PathfindingGraph> setupPathfindingSetupTests(
     tileLength = 10;
     enemyLength = 5;
     break;
+  case 10:
+    tileLength = 10;
+    enemyLength = 1;
   default:
     tileLength = enemyLength = 10;
     break;
@@ -316,6 +319,12 @@ std::unique_ptr<PathfindingGraph> setupPathfindingSetupTests(
   case 4: case 8:
     area = sf::IntRect(0, 0, 30, 20);
     break;
+  case 9:
+    area = sf::IntRect(0, 0, 50, 40);
+    break;
+  case 10:
+    area = sf::IntRect(0, 0, 60, 60);
+    break;
   default:
     std::cerr << "Invalid case number\n";
     assert(false); // shouldn't be reached; no logical default area
@@ -332,6 +341,20 @@ std::unique_ptr<PathfindingGraph> setupPathfindingSetupTests(
     tileMap += "00w00";
     tileMap += "00000";
     tileMap += "00ww0";
+    break;
+  case 9:
+    tileMap += "00000";
+    tileMap += "0ww00";
+    tileMap += "000w0";
+    tileMap += "00000";
+    break;
+  case 10:
+    tileMap += "000000";
+    tileMap += "00w000";
+    tileMap += "000000";
+    tileMap += "000w00";
+    tileMap += "0w000w";
+    tileMap += "00000w";
     break;
   default:
     tileMap = createTileMapAllDefault(area, tileLength);
@@ -376,6 +399,18 @@ std::unique_ptr<PathfindingGraph> setupPathfindingSetupTests(
     break;
   case 8:
     pg->setSearchStartOrEnd(false, sf::Vector2f(15, 10),
+      enemyLength, enemyLength);
+    break;
+  case 9:
+    pg->setSearchStartOrEnd(true, sf::Vector2f(40, 10),
+      enemyLength, enemyLength);
+    pg->setSearchStartOrEnd(false, sf::Vector2f(20, 30),
+      enemyLength, enemyLength);
+    break;
+  case 10:
+    pg->setSearchStartOrEnd(true, sf::Vector2f(20, 30),
+      enemyLength, enemyLength);
+    pg->setSearchStartOrEnd(false, sf::Vector2f(45, 15),
       enemyLength, enemyLength);
     break;
   default:
@@ -497,7 +532,43 @@ TEST_CASE("Setting pathfinding search's end vertices #4")
 
 TEST_CASE("Setting pathfinding search's start and end vertices #1")
 {
+  std::vector<Tile::Ptr> tileGrid;
+  auto pg = setupPathfindingSetupTests(9, tileGrid);
 
+  // Assertions for start vertices
+  REQUIRE(pg->getNumSearchStartVertices() == 4);
+  auto startVertices = pg->getSearchStartVertices();
+  assertEquality(startVertices[0]->getPosition(), sf::Vector2i(30, 0));
+  assertEquality(startVertices[1]->getPosition(), sf::Vector2i(40, 0));
+  assertEquality(startVertices[2]->getPosition(), sf::Vector2i(30, 10));
+  assertEquality(startVertices[3]->getPosition(), sf::Vector2i(40, 10));
+
+  // Assertions for end vertices
+  REQUIRE(pg->getNumSearchEndVertices() == 4);
+  auto endVertices = pg->getSearchEndVertices();
+  assertEquality(endVertices[0]->getPosition(), sf::Vector2i(10, 20));
+  assertEquality(endVertices[1]->getPosition(), sf::Vector2i(20, 20));
+  assertEquality(endVertices[2]->getPosition(), sf::Vector2i(10, 30));
+  assertEquality(endVertices[3]->getPosition(), sf::Vector2i(20, 30));
+}
+
+TEST_CASE("Setting pathfinding search's start and end vertices #2")
+{
+  std::vector<Tile::Ptr> tileGrid;
+  auto pg = setupPathfindingSetupTests(10, tileGrid);
+
+  // Assertions for start vertices
+  REQUIRE(pg->getNumSearchStartVertices() == 4);
+  auto startVertices = pg->getSearchStartVertices();
+  assertEquality(startVertices[0]->getPosition(), sf::Vector2i(10, 20));
+  assertEquality(startVertices[1]->getPosition(), sf::Vector2i(20, 20));
+  assertEquality(startVertices[2]->getPosition(), sf::Vector2i(10, 30));
+  assertEquality(startVertices[3]->getPosition(), sf::Vector2i(20, 30));
+
+  // Assertions for end vertices
+  REQUIRE(pg->getNumSearchEndVertices() == 1);
+  auto endVertices = pg->getSearchEndVertices();
+  assertEquality(endVertices[0]->getPosition(), sf::Vector2i(40, 10));
 }
 
 
